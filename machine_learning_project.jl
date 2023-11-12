6e09a973f3f2afab7a49fdff1c6e3ea117abc7e1
ESkoufris <eskoufris@gmail.com>
2023-11-12 18:22:23 +1000

    halfway done with task 2

--- machine_learning_project.jl
+++ machine_learning_project.jl
@@ -1,2 +1,2 @@
 
-using DataFrames, HTTP, CSV, Plots, StatsPlots, StatsBase, GLM
+using DataFrames, HTTP, CSV, Plots, StatsPlots, StatsBase, GLM, Statistics, MLDatasets, Measures, LaTeXStrings, LinearAlgebra, Random, Flux
